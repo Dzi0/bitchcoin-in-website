a6aa7a18b32c950efa719cbdf69f362c6668a9d1
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2018-03-10 12:06:33 +0300

    production deploy scripts

--- upload-to-bitchcoin-in.sh
+++ upload-to-bitchcoin-in.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+rm -f css/app.css
+rm -rf resources/public/js/compiled/*
+rm -f main.js
+
 sass -I bower_components/foundation-sites/scss -I css css/app.scss css/app.css
 
-lein cljsbuild once
+lein with-profile production cljsbuild once
+# lein cljsbuild once
 
-for i in bower_components di-songs.html fonts index.html js css main.js
+for i in resources bower_components di-songs.html fonts index.html js css main.js
 do
   rsync -arzv "$i" deploy@myfutures.trade:/var/www/bitchcoin-in-production
 done
