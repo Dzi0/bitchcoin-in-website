7007bc3f4ec02c09bd6fc64b25589287d2d3eade
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2018-03-10 23:40:46 +0300

    [deploy] removed /main.js

--- upload-to-bitchcoin-in.sh
+++ upload-to-bitchcoin-in.sh
@@ -9,7 +9,7 @@ sass -I bower_components/foundation-sites/scss -I css css/app.scss css/app.css
 lein with-profile production cljsbuild once
 # lein cljsbuild once
 
-for i in resources bower_components di-songs.html fonts index.html js css main.js
+for i in resources bower_components di-songs.html fonts index.html js css
 do
   rsync -arzv "$i" deploy@myfutures.trade:/var/www/bitchcoin-in-production
 done
